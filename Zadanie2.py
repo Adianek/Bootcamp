ad61815dd6ad7e32f0b355f9455faa23708790be
Adam <aswidzikowski@gmail.com>
2018-12-08 11:09:35 +0100

    init

---
+++ Zadanie2.py
@@ -0,0 +1,36 @@
+# Policz pole trojkata o podstawie 10 i wysokosci 5
+
+a = 10
+h = 5
+
+wynik = a/2 * h
+
+print("Pole trójkąta wynosi: ", int(wynik))
+
+# Policz pole kola o promieniu 7
+
+r = 7
+pi = 3.14
+
+wynik = pi * r**2
+
+print("Pole koła wynosi: ", wynik)
+
+# Policz pole trapezu o dlugosci podstaw 3 i 9 oraz wysokosci 5
+
+a = 3
+b = 9
+h = 5
+
+wynik = (a + b) * h / 2
+
+print("Pole trapezu wynosi: ", wynik)
+
+# Policz objetosc kuli o zadanym promieniu
+
+r = input("Podaj promień kuli: ")
+pi = 3.14
+
+wynik = 4/3 * pi * r**3
+
+print("Objętość kuli o promieniu " + str(r) + " wynosi: ", wynik)
