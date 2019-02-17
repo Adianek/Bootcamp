014a044e270d9f0da50e3a9a40c2c75285e220d5
Adam <aswidzikowski@gmail.com>
2019-02-17 16:52:42 +0100

    OK

--- Biblioteki/Zadanie_5.py
+++ Biblioteki/Zadanie_5.py
@@ -1,4 +1,12 @@
 import re
 import struct
+import json
 
-with open("\\")
+with open(r"input.txt", 'r') as f:
+    s = f.read()
+    print(s)
+
+print(re.findall(r'\d{2}-\d{3}', s))
+# print(re.findall(r'\w+', s))  wyprintowanie wszystkich napisow
+print(re.findall(r'[\w\-+.]+@[\w\-.]+', s))
+print(re.findall(r'.\d\d \w\w\w \d\d\d\d', s))
